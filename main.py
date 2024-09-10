a7a0611bf3b150e9435463c729606e2b5014d678
Yinglong-Wang <150312020+dauing@users.noreply.github.com>
2024-09-10 23:16:50 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+import torch
+import argparse
+from torch.backends import cudnn
+from model import build_net
+from train import _train
+from eval import _eval
+import numpy as np
+import random
+
+def main(args):
+    # CUDNN
+    cudnn.benchmark = True
+
+    if not os.path.exists(args.model_save_dir):
+        os.makedirs(args.model_save_dir)
+    if not os.path.exists(args.result_dir):
+        os.makedirs(args.result_dir)
+
+    model = build_net()
+
+    if torch.cuda.is_available():
+        model.cuda()
+    if args.mode == 'train':
+        _train(model, args)
+
+    elif args.mode == 'test':
+        _eval(model, args)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--mode', default='train', choices=['train', 'test'], type=str)
+    parser.add_argument('--data_dir', type=str, default='../Haze4K/Haze4K')
+
+    # Train
+    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--learning_rate', type=float, default=1e-4)
+    parser.add_argument('--weight_decay', type=float, default=0)
+    parser.add_argument('--num_epoch', type=int, default=1000)
+    parser.add_argument('--print_freq', type=int, default=100)
+    parser.add_argument('--num_worker', type=int, default=16)
+    parser.add_argument('--save_freq', type=int, default=100)
+    parser.add_argument('--valid_freq', type=int, default=10)
+    parser.add_argument('--resume', type=str, default='results/model.pkl')
+
+
+    # Test
+    parser.add_argument('--test_model', type=str, default='')
+    parser.add_argument('--save_image', type=bool, default=False, choices=[True, False])
+
+    args = parser.parse_args()
+    args.model_save_dir = os.path.join('results/')
+    args.result_dir = os.path.join('results/','test')
+    if not os.path.exists(args.model_save_dir):
+        os.makedirs(args.model_save_dir)
+    main(args)
